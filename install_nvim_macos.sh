1d531d006c4a0764fffd1d42c86110ca1f48c89a
Martin Haan <mhaan@f1r2s18.codam.nl>
2023-07-11 13:09:24 +0200

    small improvement

--- install_nvim_macos.sh
+++ install_nvim_macos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -eu
 nvim="$HOME/nvim-macos.tar.gz"
 nvimurl="https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
-curl -fL "$nvimurl" -o "$nvim"
-xattr -c $HOME/nvim-macos.tar.gz
-tar xzvf $HOME/nvim-macos.tar.gz
+curl -L "$nvimurl" -o $nvim
+xattr -c $nvim
+tar xzvf $nvim -C $HOME
+export PATH="$HOME/nvim-macos/bin:$PATH"
+rm -rf $nvim
