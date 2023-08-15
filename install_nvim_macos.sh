4f5678603151f9503e6b7e8990c74aed74b78754
Martin Haan <mhaan@f1r3s4.codam.nl>
2023-08-15 10:41:49 +0200

    added check to see if export is already in .zshrc file

--- install_nvim_macos.sh
+++ install_nvim_macos.sh
@@ -4,5 +4,6 @@ nvimurl="https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.t
 curl -L "$nvimurl" -o $nvim
 xattr -c $nvim
 tar xzvf $nvim -C $HOME
-export PATH="$HOME/nvim-macos/bin:$PATH"
+LINE="export PATH=\"HOME
+grep -qxF "export PATH=\"$HOME/nvim-macos/bin:$PATH\"" $HOME/.zshrc || echo 'export PATH="$HOME/nvim-macos/bin:$PATH"' >>$HOME/.zshrc
 rm -rf $nvim
