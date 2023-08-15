d899e0a52d63fe4d10fd7fb59402b86f4ecf2f52
Martin Haan <mhaan@f1r3s4.codam.nl>
2023-08-15 11:28:49 +0200

    added source .zshrc

--- install_nvim_macos.sh
+++ install_nvim_macos.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 nvim="$HOME/nvim-macos.tar.gz"
 nvimurl="https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz"
 curl -L "$nvimurl" -o $nvim
 xattr -c $nvim
 tar xzvf $nvim -C $HOME
-LINE="export PATH=\"HOME
-grep -qxF "export PATH=\"$HOME/nvim-macos/bin:$PATH\"" $HOME/.zshrc || echo 'export PATH="$HOME/nvim-macos/bin:$PATH"' >>$HOME/.zshrc
+grep -qxF "export PATH=\"$HOME/nvim-macos/bin:\$PATH\"" $HOME/.zshrc || echo "export PATH=\"$HOME/nvim-macos/bin:\$PATH\"" >>$HOME/.zshrc
+source $HOME/.zshrc
 rm -rf $nvim
